c1136dac4d689113ba00f8f6ec52c9a03b8cb7c0
imzhai <jue.yguel@gmail.com>
2019-08-08 11:05:51 +0200

    ajout pdo

--- 10-superheroes/SuperHeroe.php
+++ 10-superheroes/SuperHeroe.php
@@ -6,6 +6,9 @@ class SuperHeroe
     public $power;
     public $identity;
     public $universe;
+    // Un attribut statique est conservé par toutes les instances
+    public static $heroes = [];
+    
 
     public function __construct($name = null, $power = null, $identity = null, $universe = null)
     {
@@ -13,6 +16,8 @@ class SuperHeroe
         $this->power = $power;
         $this->identity = $identity;
         $this->universe = $universe;
+        // $this représente le SuperHeroe qui vient d'être créé
+        self::$heroes[] = $this;
     }
 
     public function getRealIdentity()
@@ -23,5 +28,14 @@ class SuperHeroe
     public function getUniverse()
     {
         return $this->name.' fait partie de l\'univers '.$this->universe;
+    }
+    /***
+     * La fonction doit être appelée sans avoir une instance de SuperHeroe -> ( instance) là, on utilisera ::
+     */
+    public static function all()
+    {
+        // Self est la même chose que SuperHero
+        // Les :: permettent d'accéder à un attribut statique
+        return self::$heroes;
     }
 }
--- 10-superheroes/index.php
+++ 10-superheroes/index.php
@@ -1,5 +1,9 @@
 <?php
 
+
+
+
+
 /**
  * 1/ Créer une classe SuperHeroe avec les attributs name, power, identity et universe.
  */
@@ -15,16 +19,47 @@ $ironMan->universe = 'Marvel';
 $captainAmerica = new SuperHeroe('Captain America', 'Force', 'Steve Rogers', 'Marvel');
 $hulk = new SuperHeroe('Hulk', 'Force', 'Bruce Banner', 'Marvel');
 $batman = new SuperHeroe('Batman', 'Riche', 'Bruce Wayne', 'DC');
+$spiderMan = new SuperHeroe('Spider-Man', 'Souple', 'Peter Parker', 'Marvel');
+
 
 echo $hulk->getRealIdentity(); // L'identité réelle de Hulk est Bruce Banner
 echo $hulk->getUniverse(); // Hulk fait partie de l'univers Marvel
 
-$heroes = [$ironMan, $captainAmerica, $hulk, $batman];
-var_dump($heroes);
+ // $heroes = [$ironMan, $captainAmerica, $hulk, $batman];
+// var_dump($heroes);
+
+var_dump(SuperHeroe::all());
 
 /**
  * 2/
  * Créer la base de données : superheroes
  * Créer la table : superheroe
  * Créer les colonnes : name (VARCHAR), power (VARCHAR), identity (VARCHAR) et universe (VARCHAR)
+ * 
+ * 3/ Créer connexion avec la base de données en utilisant PDO
+ * faire une première requète pour insérer le héros suivant
  */
+
+ // Connexion avec PDO
+
+try
+{
+    $db = new PDO('mysql:host=localhost;dbname=wf3_superheroes;charset=utf8', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING ]); // Activer les erreurs MySQL
+}
+catch(Exception $e)
+{
+    echo $e->getMessage();
+}
+ // La requête pour insérer le héros 
+$sql = "INSERT INTO `superheroe` (name, power, identity, universe) VALUES (:name, :power, :identity, :universe)";
+  $query = $db->prepare($sql);
+
+  $query->bindValue(':name', 'Captain America');
+  $query->bindValue(':power', 'Force');
+  $query->bindValue(':identity', 'Steve Rogers');
+  $query->bindValue(':universe', 'Marvel');
+
+  $query->execute();
+
+  
